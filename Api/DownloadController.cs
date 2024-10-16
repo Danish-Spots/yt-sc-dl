8e1c1bebb2540e8edf519caeeadf08ba3bc7d365
Danish-Spots <danishspots@gmail.com>
2024-10-16 19:12:52 +0200

    Working extractor of some metadata

--- Api/DownloadController.cs
+++ Api/DownloadController.cs
@@ -2,97 +2,166 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
 
-    /// <summary>
-    /// Api endpoint to get version of yt-dlp installed
-    /// </summary>
-    [ApiController]
-    [Route("api/[controller]")]
-    public class DownloadController : ControllerBase
+/// <summary>
+/// Api endpoint to get version of yt-dlp installed
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public class DownloadController : ControllerBase
+{
+    private string command = "/opt/venv/bin/yt-dlp";
+
+    [HttpGet("status")]
+    public IActionResult StatusCheck()
     {
-        private string command ="/opt/venv/bin/yt-dlp";
+        string arguments = "--version";
 
-        [HttpGet("status")]
-        public IActionResult StatusCheck()
+        ProcessStartInfo psi = new ProcessStartInfo
         {
-            string arguments = "--version";
+            FileName = command,
+            Arguments = arguments,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
 
-            ProcessStartInfo psi = new ProcessStartInfo
-            {
-               FileName = command,
-                Arguments = arguments,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
+        };
 
-            };
-
-            try 
+        try
+        {
+            using (Process process = Process.Start(psi))
             {
-                using(Process process = Process.Start(psi))
-                {
-                    string result = process.StandardOutput.ReadToEnd();
-                    process.WaitForExit();
-                    return Ok(new {CommandVersion = result});
-                }
-            }
-            catch (Exception e)
-            {
-                return StatusCode(500, new { Message = "Server error", Error = e.Message });
+                string result = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+                return Ok(new { CommandVersion = result });
             }
         }
-
-        [HttpPost("download-audio")]
-        public IActionResult DownloadAudio([FromBody] DownloadRequest request)
+        catch (Exception e)
         {
-            string videoUrl = request.VideoUrl;
-
-            // Specify the output path for the downloaded audio file
-            string outputDirectory = Path.Combine(Directory.GetCurrentDirectory(), "downloads");
-            string outputFilePath = Path.Combine(outputDirectory, $"{Path.GetFileNameWithoutExtension(videoUrl)}.flac");
-
-            // yt-dlp command and arguments
-            string arguments = $"-x --embed-metadata --embed-thumbnail --audio-format flac --audio-quality 0 -o \"{outputFilePath}\" {videoUrl}";
-
-            // Start the process
-            ProcessStartInfo psi = new ProcessStartInfo
-            {
-                FileName = command,
-                Arguments = arguments,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-
-            try
-            {
-                using (Process process = Process.Start(psi))
-                {
-                    process.WaitForExit();
-                    if (process.ExitCode == 0 && System.IO.File.Exists(outputFilePath))
-                    {
-                        // Return the downloaded file as a response
-                        var fileBytes = System.IO.File.ReadAllBytes(outputFilePath);
-                        var fileName = Path.GetFileName(outputFilePath);
-                        return File(fileBytes, "audio/mpeg", fileName);
-                    }
-                    else
-                    {
-                        // Handle errors and return appropriate response
-                        string error = process.StandardOutput.ReadToEnd();
-                        return BadRequest(new { Message = "Download failed", Error = error });
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new { Message = "Server error", Error = ex.Message });
-            }
+            return StatusCode(500, new { Message = "Server error", Error = e.Message });
         }
     }
 
-    public class DownloadRequest
+    [HttpPost("video-data")]
+    public IActionResult GetVideoData([FromBody] DownloadRequest request)
     {
-        public string VideoUrl { get; set; }
+        string arguments = $"--list-thumbnails --print title --print id {request.VideoUrl}";
+
+        ProcessStartInfo psi = new ProcessStartInfo
+        {
+            FileName = command,
+            Arguments = arguments,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+
+        };
+
+        try
+        {
+            using (Process process = Process.Start(psi))
+            {
+                process.WaitForExit();
+
+                string[] result = process.StandardOutput.ReadToEnd().Split("\n");
+                VideoDataResult resultObj = new VideoDataResult();
+                resultObj.images = new List<ImageDto>();
+                foreach (var item in result)
+                {
+                    // Hack for identifying title, thumbnails, id
+                    if (item.Contains(".com"))
+                    {
+                        string[] imageData = item.Split([' '], StringSplitOptions.RemoveEmptyEntries);
+                        resultObj.images.Add(new ImageDto(imageData[0].Trim(), imageData[1].Trim(), imageData[2].Trim(), imageData[3].Trim()));
+                    }
+                    else if (item.Contains(" "))
+                    {
+                        resultObj.title = item.Trim();
+                    }
+                    else if (!String.IsNullOrWhiteSpace(item))
+                    {
+                        resultObj.id = item;
+                    }
+                }
+
+                return Ok(resultObj);
+            }
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, new { Message = "Server error", Error = e.Message });
+        }
     }
+
+    [HttpPost("download-audio")]
+    public IActionResult DownloadAudio([FromBody] DownloadRequest request)
+    {
+        string videoUrl = request.VideoUrl;
+
+        // Specify the output path for the downloaded audio file
+        string outputDirectory = Path.Combine(Directory.GetCurrentDirectory(), "downloads");
+        string outputFilePath = Path.Combine(outputDirectory, $"{Path.GetFileNameWithoutExtension(videoUrl)}.flac");
+
+        // yt-dlp command and arguments
+        string arguments = $"-x --embed-metadata --embed-thumbnail --audio-format flac --audio-quality 0 -o \"{outputFilePath}\" {videoUrl}";
+
+        // Start the process
+        ProcessStartInfo psi = new ProcessStartInfo
+        {
+            FileName = command,
+            Arguments = arguments,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        try
+        {
+            using (Process process = Process.Start(psi))
+            {
+                process.WaitForExit();
+                if (process.ExitCode == 0 && System.IO.File.Exists(outputFilePath))
+                {
+                    // Return the downloaded file as a response
+                    var fileBytes = System.IO.File.ReadAllBytes(outputFilePath);
+                    var fileName = Path.GetFileName(outputFilePath);
+                    return File(fileBytes, "audio/mpeg", fileName);
+                }
+                else
+                {
+                    // Handle errors and return appropriate response
+                    string error = process.StandardOutput.ReadToEnd();
+                    return BadRequest(new { Message = "Download failed", Error = error });
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { Message = "Server error", Error = ex.Message });
+        }
+    }
+}
+
+public class DownloadRequest
+{
+    public string VideoUrl { get; set; }
+}
+
+public class ImageDto(string _ID, string _Width, string _Height, string _Url)
+{
+    // ID Width   Height  URL
+    public string ID { get; } = _ID;
+    public string Width { get; } = _Width;
+    public string Height { get; } = _Height;
+    public string Url { get; } = _Url;
+}
+
+
+public class VideoDataResult
+{
+    public string title { get; set; }
+    public List<ImageDto> images { get; set; }
+    public string id { get; set; }
+}
